aa9974c25dd3c137f37516cc1fd3af8401941fea
Matthew Watkins <matthew.watkins@channeladvisor.com>
2017-08-07 13:30:43 -0400

    Nucleotide soluition

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 
 public class DNA
 {
+    public IDictionary<char, int> NucleotideCounts { get; private set; } = new Dictionary<char, int>
+    {
+        ['A'] = 0,
+        ['T'] = 0,
+        ['C'] = 0,
+        ['G'] = 0
+    };
+    
     public DNA(string sequence)
     {
-    }
-
-    public IDictionary<char, int> NucleotideCounts
-    {
-        get
+        foreach (var c in sequence)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            NucleotideCounts[c]++;
         }
     }
 
     public int Count(char nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!NucleotideCounts.ContainsKey(nucleotide))
+        {
+            throw new InvalidNucleotideException();
+        }
+        return NucleotideCounts[nucleotide];
     }
 }
 
