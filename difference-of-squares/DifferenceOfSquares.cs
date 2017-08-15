443ab3af075e38ba88333bb8adbc789fd063a316
Matthew Watkins <matthew.watkins@channeladvisor.com>
2017-08-15 11:17:56 -0400

    Solved sum of squares

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class Squares
+{
+    public static int SquareOfSums(int max)
+    {
+        return (int)Math.Pow(Enumerable.Range(1, max).Sum(i => i), 2);
+    }
+
+    public static int SumOfSquares(int max)
+    {
+        return (int)Enumerable.Range(1, max).Sum(i => Math.Pow(i, 2));
+    }
+
+    public static int DifferenceOfSquares(int max)
+    {
+        return SquareOfSums(max) - SumOfSquares(max);
+    }
+}
