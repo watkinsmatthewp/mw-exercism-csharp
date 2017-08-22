eb5d0a7a395fb86f245f1b019e3b428d342fe42c
Matthew Watkins <watkinsmatthewp@gmail.com>
2017-08-22 16:25:31 -0400

    Solved grains

---
+++ grains/Grains.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    private const int SQUARES_IN_BOARD = 64;
+    
+    public static ulong Square(int n) => (ulong)Math.Pow(2, n-1);
+
+    public static ulong Total() => (ulong)Math.Pow(2, SQUARES_IN_BOARD) - 1;
+}
